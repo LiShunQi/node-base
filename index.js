537046c364308615e51c2dcdb8b2cb89b9a182ca
LiShunQi <ggshun7@126.com>
2018-01-09 14:11:36 +0800

    ajax formdata sc file

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 app.use(require('body-parser')());//处理表单上传数据
 var routes = require('./routes/index');
+var path = require('path');
 
-app.set('port',process.env.Port || 3000);
+app.set('port',process.env.Port || 3333);
 
 //静态资源目录
 app.use(express.static(__dirname + '/public'));
+app.use('/static',express.static(path.join(__dirname + '/static'))); //文件上传存放位置
+
 //路由
 app.use('/',routes);
 /**
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,9 @@
  */
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 var formidable = require('formidable');//处理上传的文件
 
-//首页
-router.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Index home');
-});
 //test Ajax\
 router.post('/testAjax',function (req,res) {
     console.log(req.body);
@@ -52,4 +48,50 @@ router.post('/uploadFile',function (req,res) {
         // res.redirect(303, '/thank-you');
     });
 });
+//form + iframe 上传文件(异步无刷新，可跨域)
+router.post('/formUpload',function (req, res) {
+   var form  = new formidable.IncomingForm();
+    var param_name,param_file;
+    form.uploadDir = './static/file'; //上传文件临时文件存放处
+
+    form.on('fileBegin', function(name,file) {
+        file.path = './static/file/' + file.name;
+        param_name = name;
+        param_file = file;
+    });
+    form.parse(req, function (err, fields, files) {
+        res.type('text/plain');
+        res.send({
+            fields:fields,
+            files:files,
+            param_name: param_name,
+            param_file: param_file
+        });
+    });
+});
+//ajax + formdata
+router.post('/ajaxforformdata', function (req, res) {
+   var form = new formidable.IncomingForm();
+    var param_name,param_file = [];
+    form.uploadDir = './static/file'; //上传文件临时文件存放处
+
+   form.on('fileBegin', function (name, file) {
+       file.path = path.join('./static/file/',file.name);
+       var fileList = {};
+       fileList.path = '/static/file/' + file.name;
+       fileList.type = file.type;
+       param_name = name;
+       param_file.push(fileList);
+   });
+
+   form.parse(req, function (err, fields, files) {
+       res.send({
+           fields:fields,
+           files:files,
+           param_name: param_name,
+           param_file: param_file
+       })
+   });
+});
+
 module.exports = router;
